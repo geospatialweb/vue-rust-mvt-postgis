9e72ca7b0ddfd6cd413973bc170ae1c3c3b45d19
John Campbell <johncampbell@geospatialweb.ca>
2023-12-08 04:44:36 -0500

    refactor handler lib

--- rust/src/lib/handler/mod.rs
+++ rust/src/lib/handler/mod.rs
@@ -1,31 +1,10 @@
 use super::env::Config;
-use salvo::macros::Extractible;
-use salvo::prelude::*;
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Extractible, Serialize, Deserialize)]
-#[salvo(extract(default_source(from = "query"), default_source(from = "body")))]
-struct Credentials {
-    password: String,
-    username: String,
-}
-
-#[derive(Debug, Extractible, Serialize, Deserialize)]
-#[salvo(extract(default_source(from = "query")))]
-struct Layer {
-    columns: String,
-    table: String,
-}
-
-#[derive(Debug, Extractible, Serialize, Deserialize)]
-#[salvo(extract(default_source(from = "query")))]
-struct User {
-    username: String,
-}
+use super::model::{LayerParams, User};
+use salvo::prelude::{handler, Request, Response};
 
 #[handler]
 pub async fn get_geojson_feature_collection(req: &mut Request, res: &mut Response) {
-    let params: Layer = req.extract().await.unwrap();
+    let params: LayerParams = req.extract().await.unwrap();
     println!("{params:?}");
     res.render("geojsonFeatureCollection");
 }
@@ -38,35 +17,35 @@ pub async fn get_mapbox_access_token(res: &mut Response) {
 
 #[handler]
 pub async fn delete_user(req: &mut Request, res: &mut Response) {
-    let username: User = req.extract().await.unwrap();
-    println!("{username:?}");
+    let user: User = req.extract().await.unwrap();
+    println!("{user:?}");
     res.render("deleteUser");
 }
 
 #[handler]
 pub async fn get_user(req: &mut Request, res: &mut Response) {
-    let username: User = req.extract().await.unwrap();
-    println!("{username:?}");
+    let user: User = req.extract().await.unwrap();
+    println!("{user:?}");
     res.render("getUser");
 }
 
 #[handler]
 pub async fn login_user(req: &mut Request, res: &mut Response) {
-    let credentials: Credentials = req.extract().await.unwrap();
-    println!("{credentials:?}");
+    let user: User = req.extract().await.unwrap();
+    println!("{user:?}");
     res.render("loginUser");
 }
 
 #[handler]
 pub async fn register_user(req: &mut Request, res: &mut Response) {
-    let credentials: Credentials = req.extract().await.unwrap();
-    println!("{credentials:?}");
+    let user: User = req.extract().await.unwrap();
+    println!("{user:?}");
     res.render("registerUser");
 }
 
 #[handler]
 pub async fn update_password(req: &mut Request, res: &mut Response) {
-    let credentials: Credentials = req.extract().await.unwrap();
-    println!("{credentials:?}");
+    let user: User = req.extract().await.unwrap();
+    println!("{user:?}");
     res.render("updatePassword");
 }
