2c4879ad1c9bc639fbf60cb4590036b9086df1fe
John Campbell <johncampbell@geospatialweb.ca>
2023-12-08 09:14:14 -0500

    add serde_json dependency & refactor code

--- rust/src/lib/handler/mod.rs
+++ rust/src/lib/handler/mod.rs
@@ -1,51 +1,46 @@
 use super::env::Config;
 use super::model::{LayerParams, User};
 use salvo::prelude::{handler, Request, Response};
+use serde_json;
 
 #[handler]
 pub async fn get_geojson_feature_collection(req: &mut Request, res: &mut Response) {
     let params: LayerParams = req.extract().await.unwrap();
-    println!("{params:?}");
-    res.render("geojsonFeatureCollection");
+    res.render(serde_json::to_string(&params).unwrap());
 }
 
 #[handler]
 pub async fn get_mapbox_access_token(res: &mut Response) {
     let env: Config = Default::default();
-    res.render(env.mapbox_access_token);
+    res.render(serde_json::to_string(&env.mapbox_access_token).unwrap());
 }
 
 #[handler]
 pub async fn delete_user(req: &mut Request, res: &mut Response) {
     let user: User = req.extract().await.unwrap();
-    println!("{user:?}");
-    res.render("deleteUser");
+    res.render(serde_json::to_string(&user).unwrap());
 }
 
 #[handler]
 pub async fn get_user(req: &mut Request, res: &mut Response) {
     let user: User = req.extract().await.unwrap();
-    println!("{user:?}");
-    res.render("getUser");
+    res.render(serde_json::to_string(&user).unwrap());
 }
 
 #[handler]
 pub async fn login_user(req: &mut Request, res: &mut Response) {
     let user: User = req.extract().await.unwrap();
-    println!("{user:?}");
-    res.render("loginUser");
+    res.render(serde_json::to_string(&user).unwrap());
 }
 
 #[handler]
 pub async fn register_user(req: &mut Request, res: &mut Response) {
     let user: User = req.extract().await.unwrap();
-    println!("{user:?}");
-    res.render("registerUser");
+    res.render(serde_json::to_string(&user).unwrap());
 }
 
 #[handler]
 pub async fn update_password(req: &mut Request, res: &mut Response) {
     let user: User = req.extract().await.unwrap();
-    println!("{user:?}");
-    res.render("updatePassword");
+    res.render(serde_json::to_string(&user).unwrap());
 }
