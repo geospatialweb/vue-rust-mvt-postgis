9b53dfc7cc39b7a1355dc78562bf5b4d3c5d367d
John Campbell <johncampbell@geospatialweb.ca>
2023-12-08 21:27:00 -0500

    add server OpenSSL

--- rust/src/bin/main.rs
+++ rust/src/bin/main.rs
@@ -1,17 +1,38 @@
 use dotenvy::dotenv;
 use lib::env::Config;
 use lib::router;
+use salvo::conn::openssl::{Keycert, OpensslConfig};
 use salvo::prelude::*;
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt().init();
     if let Err(err) = dotenv() {
         println!("dotenv error: {}", err);
         return;
     }
+    tracing_subscriber::fmt().init();
     let env: Config = Default::default();
-    let acceptor = TcpListener::new(format!("{}:{}", env.server_host, env.server_port)).bind().await;
-    let router = router::new();
-    Server::new(acceptor).serve(router).await;
+    let host = format!("{}:{}", env.server_host, env.server_port);
+    if env.app_mode == env.app_mode_dev {
+        let acceptor = TcpListener::new(&host)
+            .bind()
+            .await;
+        Server::new(acceptor)
+            .serve(router::new())
+            .await;
+    }
+    if env.app_mode == env.app_mode_prod {
+        let config = OpensslConfig::new(
+            Keycert::new()
+                .with_cert(env.ssl_cert)
+                .with_key(env.ssl_key)
+        );
+        let acceptor = TcpListener::new(&host)
+            .openssl(config)
+            .bind()
+            .await;
+        Server::new(acceptor)
+            .serve(router::new())
+            .await;
+    }
 }
