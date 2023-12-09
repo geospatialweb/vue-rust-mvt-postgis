14b87a50b60a0e412f4bd41ef97ba0682186e046
John Campbell <johncampbell@geospatialweb.ca>
2023-12-08 21:22:13 -0500

    remove blank lines

--- rust/src/lib/handler/mod.rs
+++ rust/src/lib/handler/mod.rs
@@ -8,37 +8,31 @@ pub async fn get_geojson_feature_collection(req: &mut Request, res: &mut Respons
     let params: LayerParams = req.extract().await.unwrap();
     res.render(serde_json::to_string(&params).unwrap());
 }
-
 #[handler]
 pub async fn get_mapbox_access_token(res: &mut Response) {
     let env: Config = Default::default();
     res.render(serde_json::to_string(&env.mapbox_access_token).unwrap());
 }
-
-#[handler]
-pub async fn delete_user(req: &mut Request, res: &mut Response) {
-    let user: User = req.extract().await.unwrap();
-    res.render(serde_json::to_string(&user).unwrap());
-}
-
 #[handler]
 pub async fn get_user(req: &mut Request, res: &mut Response) {
     let user: User = req.extract().await.unwrap();
     res.render(serde_json::to_string(&user).unwrap());
 }
-
 #[handler]
-pub async fn login_user(req: &mut Request, res: &mut Response) {
+pub async fn delete_user(req: &mut Request, res: &mut Response) {
     let user: User = req.extract().await.unwrap();
     res.render(serde_json::to_string(&user).unwrap());
 }
-
 #[handler]
-pub async fn register_user(req: &mut Request, res: &mut Response) {
+pub async fn login(req: &mut Request, res: &mut Response) {
     let user: User = req.extract().await.unwrap();
     res.render(serde_json::to_string(&user).unwrap());
 }
-
+#[handler]
+pub async fn register(req: &mut Request, res: &mut Response) {
+    let user: User = req.extract().await.unwrap();
+    res.render(serde_json::to_string(&user).unwrap());
+}
 #[handler]
 pub async fn update_password(req: &mut Request, res: &mut Response) {
     let user: User = req.extract().await.unwrap();
