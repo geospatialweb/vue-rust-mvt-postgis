42236fc64fee2f9a79b42963cdcdeee8d2750f04
John Campbell <johncampbell@geospatialweb.ca>
2023-12-10 00:49:01 -0500

    refactor main

--- rust/src/bin/main.rs
+++ rust/src/bin/main.rs
@@ -1,8 +1,18 @@
 use dotenvy::dotenv;
 use lib::env::Config;
 use lib::router;
+use once_cell::sync::OnceCell;
 use salvo::conn::openssl::{Keycert, OpensslConfig};
 use salvo::prelude::*;
+use sqlx::{PgPool, Pool};
+
+pub static PG_POOL: OnceCell<PgPool> = OnceCell::new();
+
+// #[inline]
+pub fn get_pg_pool() -> &'static PgPool {
+    // unsafe { PG_POOL.get_unchecked() }
+    PG_POOL.get().unwrap()
+}
 
 #[tokio::main]
 async fn main() {
@@ -13,26 +23,17 @@ async fn main() {
     tracing_subscriber::fmt().init();
     let env: Config = Default::default();
     let host = format!("{}:{}", env.server_host, env.server_port);
+    let pool = Pool::connect(&env.postgres_dsn).await.unwrap();
+    PG_POOL.set(pool).unwrap();
     if env.app_mode == env.app_mode_dev {
-        let acceptor = TcpListener::new(&host)
-            .bind()
-            .await;
-        Server::new(acceptor)
-            .serve(router::new())
-            .await;
+        let router = router::new();
+        let acceptor = TcpListener::new(&host).bind().await;
+        Server::new(acceptor).serve(router).await;
     }
     if env.app_mode == env.app_mode_prod {
-        let config = OpensslConfig::new(
-            Keycert::new()
-                .with_cert(env.ssl_cert)
-                .with_key(env.ssl_key)
-        );
-        let acceptor = TcpListener::new(&host)
-            .openssl(config)
-            .bind()
-            .await;
-        Server::new(acceptor)
-            .serve(router::new())
-            .await;
+        let router = router::new();
+        let config = OpensslConfig::new(Keycert::new().with_cert(env.ssl_cert).with_key(env.ssl_key));
+        let acceptor = TcpListener::new(&host).openssl(config).bind().await;
+        Server::new(acceptor).serve(router).await;
     }
 }
