ee2ad34a8f66dc904fcbf12ed43f1f1844e6885a
John Campbell <johncampbell@geospatialweb.ca>
2023-12-10 01:18:06 -0500

    move contents of rust dir to root dir

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,18 +1,18 @@
 use dotenvy::dotenv;
 use lib::env::Config;
 use lib::router;
-use once_cell::sync::OnceCell;
+// use once_cell::sync::OnceCell;
 use salvo::conn::openssl::{Keycert, OpensslConfig};
 use salvo::prelude::*;
-use sqlx::{PgPool, Pool};
+// use sqlx::{PgPool, Pool};
 
-pub static PG_POOL: OnceCell<PgPool> = OnceCell::new();
+// pub static PG_POOL: OnceCell<PgPool> = OnceCell::new();
 
 // #[inline]
-pub fn get_pg_pool() -> &'static PgPool {
-    // unsafe { PG_POOL.get_unchecked() }
-    PG_POOL.get().unwrap()
-}
+// pub fn get_pg_pool() -> &'static PgPool {
+//     // unsafe { PG_POOL.get_unchecked() }
+//     PG_POOL.get().unwrap()
+// }
 
 #[tokio::main]
 async fn main() {
@@ -23,8 +23,8 @@ async fn main() {
     tracing_subscriber::fmt().init();
     let env: Config = Default::default();
     let host = format!("{}:{}", env.server_host, env.server_port);
-    let pool = Pool::connect(&env.postgres_dsn).await.unwrap();
-    PG_POOL.set(pool).unwrap();
+    // let pool = Pool::connect(&env.postgres_dsn).await.unwrap();
+    // PG_POOL.set(pool).unwrap();
     if env.app_mode == env.app_mode_dev {
         let router = router::new();
         let acceptor = TcpListener::new(&host).bind().await;
