771b7f908be18481bb99eb4839a9ab49a6653e92
John Campbell <johncampbell@geospatialweb.ca>
2023-12-24 00:22:28 -0500

    add `garde` validation

--- rust/src/lib/model.rs
+++ rust/src/lib/model.rs
@@ -1,10 +1,13 @@
+use garde::Validate;
 use salvo::macros::Extractible;
 use serde::Deserialize;
 use sqlx::FromRow;
 
-#[derive(Debug, Deserialize, Extractible, FromRow)]
+#[derive(Debug, Deserialize, Extractible, FromRow, Validate)]
 #[salvo(extract(default_source(from = "query"), default_source(from = "body")))]
 pub struct User {
+    #[garde(email)]
     pub username: String,
+    #[garde(ascii)]
     pub password: Option<String>,
 }
