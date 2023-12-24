94fe6efd461b9e85d900bcd120fbfb4617ac2431
John Campbell <johncampbell@geospatialweb.ca>
2023-12-23 20:32:14 -0500

    add `auth_handler`

--- rust/src/lib/router.rs
+++ rust/src/lib/router.rs
@@ -1,8 +1,10 @@
-use salvo::cors::{ Cors, CorsHandler};
+use salvo::cors::{Cors, CorsHandler};
 use salvo::http::Method;
+use salvo::jwt_auth::{ConstDecoder, HeaderFinder};
 use salvo::logging::Logger;
-use salvo::prelude::Router;
+use salvo::prelude::{JwtAuth,Router};
 
+use super::auth::Claims;
 use super::env::Config;
 use super::handler;
 
@@ -30,46 +32,25 @@ pub fn cors() -> CorsHandler {
 
 pub fn new() -> Router {
     let env: Config = Default::default();
+    let _auth_handler: JwtAuth<Claims, _> = JwtAuth::new(ConstDecoder::from_secret(env.jwt_secret.as_bytes()))
+        .finders(vec![Box::new(HeaderFinder::new())])
+        .force_passed(true);
     Router::new()
         .hoop(cors())
         .hoop(Logger::new())
         .push(
             Router::with_path(env.api_path_prefix)
-                // .hoop(jwt_auth_middleware)
-                .push(
-                    Router::with_path(env.geojson_endpoint)
-                        .get(handler::get_geojson_feature_collection),
-                )
-                .push(
-                    Router::with_path(env.mapbox_access_token_endpoint)
-                        .get(handler::get_mapbox_access_token),
-                )
-                .push(
-                    Router::with_path(env.get_user_endpoint)
-                        .get(handler::get_user)
-                )
-                .push(
-                    Router::with_path(env.delete_user_endpoint)
-                        .delete(handler::delete_user)
-                )
-                .push(
-                    Router::with_path(env.update_password_endpoint)
-                        .patch(handler::update_password),
-                ),
+                // .hoop(auth_handler)
+                .push(Router::with_path(env.geojson_endpoint).get(handler::get_geojson_feature_collection))
+                .push(Router::with_path(env.mapbox_access_token_endpoint).get(handler::get_mapbox_access_token))
+                .push(Router::with_path(env.get_user_endpoint).get(handler::get_user))
+                .push(Router::with_path(env.delete_user_endpoint).delete(handler::delete_user))
+                .push(Router::with_path(env.update_password_endpoint).patch(handler::update_password)),
         )
         .push(
             Router::with_path(env.credentials_path_prefix)
-                .push(
-                    Router::with_path(env.validate_user_endpoint)
-                        .get(handler::get_user)
-                )
-                .push(
-                    Router::with_path(env.login_endpoint)
-                        .get(handler::login)
-                )
-                .push(
-                    Router::with_path(env.register_endpoint)
-                        .post(handler::register)
-                ),
+                .push(Router::with_path(env.validate_user_endpoint).get(handler::get_user))
+                .push(Router::with_path(env.login_endpoint).get(handler::login))
+                .push(Router::with_path(env.register_endpoint).post(handler::register)),
         )
 }
