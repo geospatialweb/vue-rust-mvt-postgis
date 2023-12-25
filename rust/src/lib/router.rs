4ecd8a19eef0729b17784247dce28e4838bb86b5
John Campbell <johncampbell@geospatialweb.ca>
2023-12-24 20:55:02 -0500

    refactoir code

--- rust/src/lib/router.rs
+++ rust/src/lib/router.rs
@@ -10,9 +10,23 @@ use super::handler;
 
 pub fn cors() -> CorsHandler {
     Cors::new()
-        .allow_origin(vec!["https://www.geospatialweb.ca", "http://localhost:4173", "http://localhost:5173"])
-        .allow_methods(vec![Method::DELETE, Method::GET, Method::OPTIONS, Method::PATCH, Method::POST])
-        .allow_headers(vec!["Accept", "Authorization", "Content-Type"])
+        .allow_origin(vec![
+            "https://www.geospatialweb.ca",
+            "http://localhost:4173",
+            "http://localhost:5173",
+        ])
+        .allow_methods(vec![
+            Method::DELETE,
+            Method::GET,
+            Method::OPTIONS,
+            Method::PATCH,
+            Method::POST,
+        ])
+        .allow_headers(vec![
+            "Accept",
+            "Authorization",
+            "Content-Type",
+        ])
         .into_handler()
 }
 
@@ -24,17 +38,33 @@ pub fn new() -> Router {
         .push(
             Router::with_path(env.api_path_prefix)
                 .hoop(auth())
-                .push(Router::with_path(env.geojson_endpoint).get(handler::get_geojson_feature_collection))
-                .push(Router::with_path(env.mapbox_access_token_endpoint).get(handler::get_mapbox_access_token))
-                .push(Router::with_path(env.get_user_endpoint).get(handler::get_user))
-                .push(Router::with_path(env.delete_user_endpoint).delete(handler::delete_user))
-                .push(Router::with_path(env.update_password_endpoint).patch(handler::update_password)),
+                .push(
+                    Router::with_path(env.geojson_endpoint)
+                        .get(handler::get_geojson_feature_collection))
+                .push(
+                    Router::with_path(env.mapbox_access_token_endpoint)
+                        .get(handler::get_mapbox_access_token))
+                .push(
+                    Router::with_path(env.get_user_endpoint)
+                        .get(handler::get_user))
+                .push(
+                    Router::with_path(env.delete_user_endpoint)
+                        .delete(handler::delete_user))
+                .push(
+                    Router::with_path(env.update_password_endpoint)
+                        .patch(handler::update_password)),
         )
         .push(
             Router::with_path(env.credentials_path_prefix)
-                .push(Router::with_path(env.validate_user_endpoint).get(handler::validate_user))
-                .push(Router::with_path(env.login_endpoint).get(handler::login))
-                .push(Router::with_path(env.register_endpoint).post(handler::register)),
+                .push(
+                    Router::with_path(env.validate_user_endpoint)
+                        .get(handler::validate_user))
+                .push(
+                    Router::with_path(env.login_endpoint)
+                        .get(handler::login))
+                .push(
+                    Router::with_path(env.register_endpoint)
+                        .post(handler::register)),
         )
 }
 
