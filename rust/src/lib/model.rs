8dde5826366c35da9923a2d1d66700741578622a
John Campbell <johncampbell@geospatialweb.ca>
2023-12-26 20:11:38 -0500

    add User fn new() impl

--- rust/src/lib/model.rs
+++ rust/src/lib/model.rs
@@ -1,13 +1,21 @@
 use garde::Validate;
 use salvo::macros::Extractible;
-use serde::Deserialize;
+use serde::{Deserialize,Serialize};
 use sqlx::FromRow;
 
-#[derive(Debug, Deserialize, Extractible, FromRow, Validate)]
+#[derive(Debug, Extractible, FromRow, Deserialize, Serialize, Validate)]
 #[salvo(extract(default_source(from = "query"), default_source(from = "body")))]
 pub struct User {
     #[garde(email)]
     pub username: String,
     #[garde(ascii)]
     pub password: Option<String>,
+}
+impl User {
+    pub fn new(username: &str, password: &Option<String>) -> Self {
+        Self {
+            username: username.to_owned(),
+            password: password.to_owned(),
+        }
+    }
 }
