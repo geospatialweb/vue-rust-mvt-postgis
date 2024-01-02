fc5cefd86eafc54a7292793bb6340aaa9ccd1ecb
John Campbell <johncampbell@geospatialweb.ca>
2024-01-02 16:34:10 -0500

    add additional auth functionality re claims and token validation

--- rust/src/lib/auth.rs
+++ rust/src/lib/auth.rs
@@ -1,12 +1,24 @@
+use bcrypt::{BcryptError, DEFAULT_COST};
 use chrono::{Duration, Utc};
 use jsonwebtoken::errors::Error;
-use jsonwebtoken::{self, EncodingKey};
+use jsonwebtoken::{DecodingKey, EncodingKey, Header, Validation};
 use salvo::jwt_auth::{ConstDecoder, HeaderFinder};
-use salvo::prelude::JwtAuth;
+use salvo::prelude::{Depot, JwtAuth, JwtAuthDepotExt};
 use serde::{Deserialize, Serialize};
 
-use super::env::Config;
-use super::model::User;
+use super::env::Env;
+
+#[derive(Debug)]
+pub struct Credential {
+    pub hash: String,
+}
+impl Credential {
+    pub fn new(password: &str) -> Self {
+        Self {
+            hash: password.to_owned(),
+        }
+    }
+}
 
 #[derive(Debug, Serialize)]
 pub struct Jwt {
@@ -22,25 +34,54 @@ pub struct JwtClaims {
 }
 
 pub fn auth() -> JwtAuth<JwtClaims, ConstDecoder> {
-    let env: Config = Default::default();
-    JwtAuth::new(ConstDecoder::from_secret(env.jwt_secret.as_bytes()))
+    let env: Env = Default::default();
+    JwtAuth::new(ConstDecoder::from_secret(env.jwt_claims_secret.as_bytes()))
         .finders(vec![Box::new(HeaderFinder::new())])
         .force_passed(true)
 }
 
-pub fn get_jwt(user: &User) -> Result<Jwt, Error> {
-    let env: Config = Default::default();
-    let jwt_expiry = env.jwt_expiry.parse::<i64>().unwrap();
-    let expiry = (Utc::now() + Duration::minutes(jwt_expiry)).timestamp();
-    let claims = JwtClaims {
-        iss: env.jwt_domain.to_owned(),
-        sub: user.username.to_owned(),
-        exp: expiry,
+pub fn generate_hash_from_password(password: &str) -> Result<String, BcryptError> {
+    let hash = bcrypt::hash(password, DEFAULT_COST)?;
+    Ok(hash)
+}
+
+pub fn verify_password_and_hash(password: &str, hash: &str) -> Result<bool, BcryptError> {
+    let verify = bcrypt::verify(password, hash)?;
+    Ok(verify)
+}
+
+pub fn get_jwt(username: &str) -> Result<Jwt, Error> {
+    let env: Env = Default::default();
+    let minutes = env.jwt_claims_expiry.parse::<i64>().unwrap();
+    let expiry = (Utc::now() + Duration::minutes(minutes)).timestamp();
+    let jwt_claims = JwtClaims {
+        iss: env.jwt_claims_issuer.to_owned(),
+        sub: username.to_owned(),
+        exp: expiry.to_owned(),
     };
     let token = jsonwebtoken::encode(
-        &jsonwebtoken::Header::default(),
-        &claims,
-        &EncodingKey::from_secret(env.jwt_secret.as_bytes()),
+        &Header::default(), // HS256
+        &jwt_claims,
+        &EncodingKey::from_secret(env.jwt_claims_secret.as_ref()),
     )?;
     Ok(Jwt { token, expiry })
+}
+
+#[rustfmt::skip]
+pub fn validate_jwt_claims_issuer(depot: &mut Depot) -> bool {
+    match depot.jwt_auth_token() {
+        Some(token) => {
+            let env: Env = Default::default();
+            let jwt_claims = jsonwebtoken::decode::<JwtClaims>(
+                token,
+                &DecodingKey::from_secret(env.jwt_claims_secret.as_ref()),
+                &Validation::default(), // HS256
+            ).unwrap();
+            if jwt_claims.claims.iss != env.jwt_claims_issuer {
+                return false;
+            }
+            true
+        }
+        None => false,
+    }
 }
