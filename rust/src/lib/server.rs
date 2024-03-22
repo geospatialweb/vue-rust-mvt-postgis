e8f3b4568a3261844ce7d513655077470bda17b5
John Campbell <johncampbell@geospatialweb.ca>
2024-03-21 20:42:39 -0400

    add server tls error checking

--- rust/src/lib/server.rs
+++ rust/src/lib/server.rs
@@ -5,7 +5,7 @@ use salvo::{
     conn::rustls::{Keycert, RustlsConfig},
     prelude::{Listener, TcpListener},
     server::{Server, ServerHandle},
-    Service,
+    Error, Service,
 };
 use tokio::signal::{self, unix};
 use tracing::{error, info};
@@ -13,40 +13,27 @@ use tracing::{error, info};
 use super::env::Env;
 use super::router;
 
-/// Set server host to service http requests.
-pub async fn set_server() {
+/// Start server host with http request service.
+pub async fn start() -> Result<(), Error> {
     let env = Env::get_env();
-    let host = format!("{}:{}", &env.server_host, &env.server_port);
-    let service = get_service();
-    if env.app_mode == env.app_mode_dev {
-        start_development_http_server(&host, service).await;
-    } else if env.app_mode == env.app_mode_prod {
-        start_production_https_server(&host, service).await;
-    } else {
-        return error!("app mode set incorrctly: {}", &env.app_mode);
-    }
-}
-
-/// Instantiate router and CORS service to handle http requests.
-fn get_service() -> Service {
-    Service::new(router::new())
+    let service = Service::new(router::new())
         .catcher(Catcher::default()
             .hoop(router::handle_cors())
-        )
-}
-
-/// Return configuration for the tls server.
-fn get_tls_config() -> RustlsConfig {
-    let env = Env::get_env();
-    RustlsConfig::new(
-        Keycert::new()
-            .cert(env.ssl_cert.as_bytes())
-            .key(env.ssl_key.as_bytes())
-    )
+        );
+    if env.app_mode == env.app_mode_dev {
+        let host = format!("{}:{}", &env.server_host_dev, &env.server_port);
+        start_http_server(&host, service).await;
+    } else if env.app_mode == env.app_mode_prod {
+        let host = format!("{}:{}", &env.server_host_prod, &env.server_port);
+        start_https_server(&host, service, &env.server_tls_cert, &env.server_tls_key).await?;
+    } else {
+        error!("app mode set incorrctly: {}", &env.app_mode);
+    }
+    Ok(())
 }
 
 // Start development HTTP/1.1 server.
-async fn start_development_http_server(host: &str, service: Service) {
+async fn start_http_server(host: &str, service: Service) {
     let acceptor = TcpListener::new(host)
         .bind()
         .await;
@@ -59,8 +46,12 @@ async fn start_development_http_server(host: &str, service: Service) {
 }
 
 // Start production HTTPS/1.1 server.
-async fn start_production_https_server(host: &str, service: Service) {
-    let tls_config = get_tls_config();
+async fn start_https_server(host: &str, service: Service, tls_cert: &str, tls_key: &str) -> Result<(), Error> {
+    let tls_config = RustlsConfig::new(
+        Keycert::new()
+            .cert_from_path(tls_cert)?
+            .key_from_path(tls_key)?
+    );
     let acceptor = TcpListener::new(host)
         .rustls(tls_config)
         .bind()
@@ -71,6 +62,7 @@ async fn start_production_https_server(host: &str, service: Service) {
     server
         .serve(service)
         .await;
+    Ok(())
 }
 
 /// Listen for shutdown signal.
