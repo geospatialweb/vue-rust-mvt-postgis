440ce4751107818c1efa1fcd601ec5d3b1bebfb3
John Campbell <johncampbell@geospatialweb.ca>
2024-03-22 20:18:27 -0400

    add server tests

--- rust/src/lib/server.rs
+++ rust/src/lib/server.rs
@@ -33,7 +33,9 @@ async fn set_server_host(service: Service) -> Result<(), Error> {
         start_http_server(&host, service).await;
     } else if env.app_mode == env.app_mode_prod {
         let host = format!("{}:{}", &env.server_host_prod, &env.server_port);
-        start_https_server(&host, service).await?;
+        let tls_cert = &env.server_tls_cert;
+        let tls_key = &env.server_tls_key;
+        start_https_server(&host, service, tls_cert, tls_key).await?;
     } else {
         error!("app mode set incorrctly: {}", &env.app_mode);
     }
@@ -54,12 +56,11 @@ async fn start_http_server(host: &str, service: Service) {
 }
 
 // Start production HTTPS/1.1 server.
-async fn start_https_server(host: &str, service: Service) -> Result<(), Error> {
-    let env = Env::get_env();
+async fn start_https_server(host: &str, service: Service, tls_cert: &str, tls_key: &str) -> Result<(), Error> {
     let tls_config = RustlsConfig::new(
         Keycert::new()
-            .cert_from_path(&env.server_tls_cert)?
-            .key_from_path(&env.server_tls_key)?
+            .cert_from_path(tls_cert)?
+            .key_from_path(tls_key)?
     );
     let acceptor = TcpListener::new(host)
         .rustls(tls_config)
@@ -92,4 +93,41 @@ async fn listen_shutdown_signal(handle: ServerHandle) {
         _ = terminate => info!("terminate signal received"),
     };
     handle.stop_graceful(None);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn get_host() -> String {
+        let env = Env::get_env();
+        format!("{}:{}", &env.server_host_prod, &env.server_port)
+    }
+
+    fn get_service() -> Service {
+        Service::new(router::new())
+            .catcher(Catcher::default()
+                .hoop(router::handle_cors())
+            )
+    }
+
+    #[tokio::test]
+    async fn tls_cert_err() {
+        let env = Env::get_env();
+        let host = get_host();
+        let service = get_service();
+        let tls_cert = "/etc/letsencrypt/fullchain.pem";
+        let result = start_https_server(&host, service, tls_cert, &env.server_tls_key).await;
+        assert!(matches!(result, Err(_)));
+    }
+
+    #[tokio::test]
+    async fn tls_key_err() {
+        let env = Env::get_env();
+        let host = get_host();
+        let service = get_service();
+        let tls_key = "/etc/letsencrypt/privkey.pem";
+        let result = start_https_server(&host, service, &env.server_tls_cert, tls_key).await;
+        assert!(matches!(result, Err(_)));
+    }
 }
