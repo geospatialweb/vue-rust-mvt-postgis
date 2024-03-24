49a31aa59ab87631b498744c610074badd630cb9
John Campbell <johncampbell@geospatialweb.ca>
2024-03-24 12:31:35 -0400

    refactor router tests

--- rust/src/lib/router.rs
+++ rust/src/lib/router.rs
@@ -37,12 +37,15 @@ pub fn handle_cors() -> CorsHandler {
 /// Create new Router.
 pub fn new() -> Router {
     let env = Env::get_env();
+    let handle_auth = auth::handle_auth();
+    let handle_cors = handle_cors();
+    let logger = Logger::new();
     Router::new()
-        .hoop(handle_cors())
-        .hoop(Logger::new())
+        .hoop(handle_cors)
+        .hoop(logger)
         .push(
             Router::with_path(&env.api_path_prefix)
-                .hoop(auth::handle_auth())
+                .hoop(handle_auth)
                 .push(
                     Router::with_path(&env.geojson_endpoint)
                         .get(handler::handle_get_geojson_feature_collection))
@@ -110,225 +113,270 @@ mod test {
     #[tokio::test]
     async fn a_register_endpoint_ok() {
         let env = Env::get_env();
+        let service = get_service();
         let body = r#"{"username":"foo@bar.com","password":"secretPassword"}"#;
         let url = format!("{}{}", &get_credentials_url(), &env.register_endpoint);
         let res = RequestBuilder::new(&url, Method::POST)
             .raw_json(body)
-            .send(&get_service())
+            .send(&service)
             .await;
-        assert_eq!(res.status_code.unwrap(), StatusCode::CREATED);
+        let status_code = res.status_code.unwrap();
+        assert_eq!(status_code, StatusCode::CREATED);
     }
 
     #[tokio::test]
     async fn b_register_endpoint_err() {
         let env = Env::get_env();
+        let service = get_service();
         let body = r#"{"username":"foo@bar.com","password":"secretPassword"}"#;
         let url = format!("{}{}", &get_credentials_url(), &env.register_endpoint);
         let res = RequestBuilder::new(&url, Method::POST)
             .raw_json(body)
-            .send(&get_service())
+            .send(&service)
             .await;
-        assert_eq!(res.status_code.unwrap(), StatusCode::INTERNAL_SERVER_ERROR);
+        let status_code = res.status_code.unwrap();
+        assert_eq!(status_code, StatusCode::INTERNAL_SERVER_ERROR);
     }
 
     #[tokio::test]
     async fn c_validate_user_endpoint_ok() {
         let env = Env::get_env();
+        let service = get_service();
         let username = "foo@bar.com";
         let url = format!("{}{}", &get_credentials_url(), &env.validate_user_endpoint);
         let res = RequestBuilder::new(&url, Method::GET)
             .query("username", username)
-            .send(&get_service())
+            .send(&service)
             .await;
-        assert_eq!(res.status_code.unwrap(), StatusCode::OK);
+        let status_code = res.status_code.unwrap();
+        assert_eq!(status_code, StatusCode::OK);
     }
 
     #[tokio::test]
     async fn d_validate_user_endpoint_err() {
         let env = Env::get_env();
+        let service = get_service();
         let username = "bar@foo.com";
         let url = format!("{}{}", &get_credentials_url(), &env.validate_user_endpoint);
         let res = RequestBuilder::new(&url, Method::GET)
             .query("username", username)
-            .send(&get_service())
+            .send(&service)
             .await;
-        assert_eq!(res.status_code.unwrap(), StatusCode::INTERNAL_SERVER_ERROR);
+        let status_code = res.status_code.unwrap();
+        assert_eq!(status_code, StatusCode::INTERNAL_SERVER_ERROR);
     }
 
     #[tokio::test]
     async fn e_login_endpoint_ok() {
         let env = Env::get_env();
+        let service = get_service();
         let username = "foo@bar.com";
         let password = "passwordSecret";
         let url = format!("{}{}", &get_credentials_url(), &env.login_endpoint);
         let res = RequestBuilder::new(&url, Method::GET)
             .queries(&[("username", username), ("password", password)])
-            .send(&get_service())
+            .send(&service)
             .await;
-        assert_eq!(res.status_code.unwrap(), StatusCode::OK);
+        let status_code = res.status_code.unwrap();
+        assert_eq!(status_code, StatusCode::OK);
     }
 
     #[tokio::test]
     async fn f_login_endpoint_err() {
         let env = Env::get_env();
+        let service = get_service();
         let username = "bar@foo.com";
         let password = "passwordSecret";
         let url = format!("{}{}", &get_credentials_url(), &env.login_endpoint);
         let res = RequestBuilder::new(&url, Method::GET)
             .queries(&[("username", username), ("password", password)])
-            .send(&get_service())
+            .send(&service)
             .await;
-        assert_eq!(res.status_code.unwrap(), StatusCode::INTERNAL_SERVER_ERROR);
+        let status_code = res.status_code.unwrap();
+        assert_eq!(status_code, StatusCode::INTERNAL_SERVER_ERROR);
     }
 
     #[tokio::test]
     async fn g_geojson_endpoint_ok() {
         let env = Env::get_env();
+        let service = get_service();
+        let token = get_jwt_token();
         let columns = "name,description,geom";
         let table = "office";
         let url = format!("{}{}", &get_api_url(), &env.geojson_endpoint);
         let res = RequestBuilder::new(&url, Method::GET)
-            .bearer_auth(&get_jwt_token())
+            .bearer_auth(&token)
             .queries(&[("columns", columns), ("table", table)])
-            .send(&get_service())
+            .send(&service)
             .await;
-        assert_eq!(res.status_code.unwrap(), StatusCode::OK);
+        let status_code = res.status_code.unwrap();
+        assert_eq!(status_code, StatusCode::OK);
     }
 
     #[tokio::test]
     async fn h_geojson_endpoint_err() {
         let env = Env::get_env();
+        let service = get_service();
+        let token = get_jwt_token();
         let columns = "name,description,geom";
         let table = "offices";
         let url = format!("{}{}", &get_api_url(), &env.geojson_endpoint);
         let res = RequestBuilder::new(&url, Method::GET)
-            .bearer_auth(&get_jwt_token())
+            .bearer_auth(&token)
             .queries(&[("columns", columns), ("table", table)])
-            .send(&get_service())
+            .send(&service)
             .await;
-        assert_eq!(res.status_code.unwrap(), StatusCode::INTERNAL_SERVER_ERROR);
+        let status_code = res.status_code.unwrap();
+        assert_eq!(status_code, StatusCode::INTERNAL_SERVER_ERROR);
     }
 
     #[tokio::test]
     async fn i_mapbox_access_token_endpoint_ok() {
         let env = Env::get_env();
+        let service = get_service();
+        let token = get_jwt_token();
         let url = format!("{}{}", &get_api_url(), &env.mapbox_access_token_endpoint);
         let res = RequestBuilder::new(&url, Method::GET)
-            .bearer_auth(&get_jwt_token())
-            .send(&get_service())
+            .bearer_auth(&token)
+            .send(&service)
             .await;
-        assert_eq!(res.status_code.unwrap(), StatusCode::OK);
+        let status_code = res.status_code.unwrap();
+        assert_eq!(status_code, StatusCode::OK);
     }
 
     #[tokio::test]
-    async fn j_mapbox_access_token_endpoint_unauthorized() {
+    async fn j_mapbox_access_token_endpoint_unauthorized_no_bearer_auth() {
         let env = Env::get_env();
+        let service = get_service();
         let url = format!("{}{}", &get_api_url(), &env.mapbox_access_token_endpoint);
         let res = RequestBuilder::new(&url, Method::GET)
-            .send(&get_service())
+            .send(&service)
             .await;
-        assert_eq!(res.status_code.unwrap(), StatusCode::UNAUTHORIZED);
+        let status_code = res.status_code.unwrap();
+        assert_eq!(status_code, StatusCode::UNAUTHORIZED);
     }
 
     #[tokio::test]
     async fn k_mapbox_access_token_endpoint_forbidden_expired_token() {
         let env = Env::get_env();
+        let service = get_service();
         let url = format!("{}{}", &get_api_url(), &env.mapbox_access_token_endpoint);
         let expired_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiYXVkIjoiZ2Vvc3BhdGlhbHdlYi5jYSIsImV4cCI6MTY3OTk0MzkyNywiaXNzIjoiZ2Vvc3BhdGlhbHdlYi5jYSIsIm5hbWUiOiJqb2huY2FtcGJlbGxAZ2Vvc3BhdGlhbHdlYi5jYSIsInN1YiI6IjEifQ.w8oD07bCbCp7h4vfJU9X4f8Pam4QRrOd-se4Pggices";
         let res = RequestBuilder::new(&url, Method::GET)
             .bearer_auth(expired_token)
-            .send(&get_service())
+            .send(&service)
             .await;
-        assert_eq!(res.status_code.unwrap(), StatusCode::FORBIDDEN);
+        let status_code = res.status_code.unwrap();
+        assert_eq!(status_code, StatusCode::FORBIDDEN);
     }
 
     #[tokio::test]
     async fn l_mapbox_access_token_endpoint_forbidden_invalid_token() {
         let env = Env::get_env();
+        let service = get_service();
         let url = format!("{}{}", &get_api_url(), &env.mapbox_access_token_endpoint);
         let invalid_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiYXVkIjoiZ2Vvc3BhdGlhbHdlYi5jYSIsImV4cCI6MTY3OTk0MzkyNywiaXNzIjoiZ2Vvc3BhdGlhbHdlYi5jYSIsIm5hbWUiOiJqb2huY2FtcGJlbGxAZ2Vvc3BhdGlhbHdlYi5jYSIsInN1YiI6IjEifQ.w8oD07bCbCp7h4vfJU9X4f8Pam4QRrOd-se4PggicesJ";
         let res = RequestBuilder::new(&url, Method::GET)
             .bearer_auth(invalid_token)
-            .send(&get_service())
+            .send(&service)
             .await;
-        assert_eq!(res.status_code.unwrap(), StatusCode::FORBIDDEN);
+        let status_code = res.status_code.unwrap();
+        assert_eq!(status_code, StatusCode::FORBIDDEN);
     }
 
     #[tokio::test]
     async fn m_get_user_endpoint_ok() {
         let env = Env::get_env();
+        let service = get_service();
+        let token = get_jwt_token();
         let username = "foo@bar.com";
         let url = format!("{}{}", &get_api_url(), &env.get_user_endpoint);
         let res = RequestBuilder::new(&url, Method::GET)
-            .bearer_auth(&get_jwt_token())
+            .bearer_auth(&token)
             .query("username", username)
-            .send(&get_service())
+            .send(&service)
             .await;
-        assert_eq!(res.status_code.unwrap(), StatusCode::OK);
+        let status_code = res.status_code.unwrap();
+        assert_eq!(status_code, StatusCode::OK);
     }
 
     #[tokio::test]
     async fn n_get_user_endpoint_err() {
         let env = Env::get_env();
+        let service = get_service();
+        let token = get_jwt_token();
         let username = "bar@foo.com";
         let url = format!("{}{}", &get_api_url(), &env.get_user_endpoint);
         let res = RequestBuilder::new(&url, Method::GET)
-            .bearer_auth(&get_jwt_token())
+            .bearer_auth(&token)
             .query("username", username)
-            .send(&get_service())
+            .send(&service)
             .await;
-        assert_eq!(res.status_code.unwrap(), StatusCode::INTERNAL_SERVER_ERROR);
+        let status_code = res.status_code.unwrap();
+        assert_eq!(status_code, StatusCode::INTERNAL_SERVER_ERROR);
     }
 
     #[tokio::test]
     async fn o_update_password_endpoint_ok() {
         let env = Env::get_env();
+        let service = get_service();
+        let token = get_jwt_token();
         let body = r#"{"username":"foo@bar.com","password":"newSecretPassword"}"#;
         let url = format!("{}{}", &get_api_url(), &env.update_password_endpoint);
         let res = RequestBuilder::new(&url, Method::PATCH)
-            .bearer_auth(&get_jwt_token())
+            .bearer_auth(&token)
             .raw_json(body)
-            .send(&get_service())
+            .send(&service)
             .await;
-        assert_eq!(res.status_code.unwrap(), StatusCode::OK);
+        let status_code = res.status_code.unwrap();
+        assert_eq!(status_code, StatusCode::OK);
     }
 
     #[tokio::test]
     async fn p_update_password_endpoint_err() {
         let env = Env::get_env();
+        let service = get_service();
+        let token = get_jwt_token();
         let body = r#"{"username":"bar@foo.com","password":"newSecretPassword"}"#;
         let url = format!("{}{}", &get_api_url(), &env.update_password_endpoint);
         let res = RequestBuilder::new(&url, Method::PATCH)
-            .bearer_auth(&get_jwt_token())
+            .bearer_auth(&token)
             .raw_json(body)
-            .send(&get_service())
+            .send(&service)
             .await;
-        assert_eq!(res.status_code.unwrap(), StatusCode::INTERNAL_SERVER_ERROR);
+        let status_code = res.status_code.unwrap();
+        assert_eq!(status_code, StatusCode::INTERNAL_SERVER_ERROR);
     }
 
     #[tokio::test]
     async fn q_delete_user_endpoint_ok() {
         let env = Env::get_env();
+        let service = get_service();
+        let token = get_jwt_token();
         let username = "foo@bar.com";
         let url = format!("{}{}", &get_api_url(), &env.delete_user_endpoint);
         let res = RequestBuilder::new(&url, Method::DELETE)
-            .bearer_auth(&get_jwt_token())
+            .bearer_auth(&token)
             .query("username", username)
-            .send(&get_service())
+            .send(&service)
             .await;
-        assert_eq!(res.status_code.unwrap(), StatusCode::OK);
+        let status_code = res.status_code.unwrap();
+        assert_eq!(status_code, StatusCode::OK);
     }
 
     #[tokio::test]
     async fn r_delete_user_endpoint_err() {
         let env = Env::get_env();
+        let service = get_service();
+        let token = get_jwt_token();
         let username = "bar@foo.com";
         let url = format!("{}{}", &get_api_url(), &env.delete_user_endpoint);
         let res = RequestBuilder::new(&url, Method::DELETE)
-            .bearer_auth(&get_jwt_token())
+            .bearer_auth(&token)
             .query("username", username)
-            .send(&get_service())
+            .send(&service)
             .await;
-        assert_eq!(res.status_code.unwrap(), StatusCode::INTERNAL_SERVER_ERROR);
+        let status_code = res.status_code.unwrap();
+        assert_eq!(status_code, StatusCode::INTERNAL_SERVER_ERROR);
     }
 }
