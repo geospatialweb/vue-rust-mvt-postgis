bfe3243c228be6ca9615f280e567225a2cfc3523
John Campbell <johncampbell@geospatialweb.ca>
2024-04-13 18:21:08 -0400

    change header title to `flex: 1` & refactor component

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -6,7 +6,7 @@ import styles from './index.module.css'
 export default defineComponent({
   name: 'Header',
   setup() {
-    const { header, name, title } = styles
+    const { header, name, repo, title } = styles
     return (): JSX.Element => (
       <header class={header}>
         <img src="/assets/icons/logo.webp" alt="Geospatial Web Logo" />
@@ -16,14 +16,16 @@ export default defineComponent({
         <div id="header-title" class={title} aria-labelledby="header-title">
           Rust&ensp;REST API&ensp;&#45;&ensp;Martin&ensp;MVT&thinsp;Tile&thinsp;Server&ensp;&#45;&ensp;PostGIS
         </div>
-        <a
-          href="https://gitlab.com/geospatialweb/rust-mvt-postgis"
-          rel="noopener noreferrer"
-          target="_blank"
-          title="Check out the source code"
-        >
-          GitLab Repository
-        </a>
+        <div id="header-repo" class={repo} aria-labelledby="header-repo">
+          <a
+            href="https://gitlab.com/geospatialweb/rust-mvt-postgis"
+            rel="noopener noreferrer"
+            target="_blank"
+            title="Check out the source code"
+          >
+            GitLab Repository
+          </a>
+        </div>
       </header>
     )
   }
