7e778a56f5031ac70b62e661eed4951717af4d01
johncampbell <johncampbell@geospatialweb.ca>
2024-06-23 19:40:31 -0400

    rename `HexagonUIHeading` component to `HexagonLayerControllerHeading`

--- vue/src/components/HexagonLayerController/HexagonLayerControllerHeading/index.tsx
+++ vue/src/components/HexagonLayerController/HexagonLayerControllerHeading/index.tsx
@@ -2,7 +2,7 @@ import 'vue/jsx'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'HexagonUIHeading Component',
+  name: 'HexagonLayerControllerHeading Component',
   setup(_, { slots }) {
     return (): JSX.Element => <h1>{slots.heading && slots.heading()}</h1>
   }
