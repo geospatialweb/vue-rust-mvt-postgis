4b7e1bebcc7334634a7e35c1e933cf4ce7551abc
johncampbell <johncampbell@geospatialweb.ca>
2024-06-23 19:28:54 -0400

    add LayerControllerLayerListItem component test

---
+++ vue/src/components/LayerController/LayerControllerLayerListItem/index.test.ts
@@ -0,0 +1,22 @@
+import { mount } from '@vue/test-utils'
+
+import { LayerControllerLayerListItem } from '@/components'
+import { testData } from '@/test'
+
+describe('LayerControllerLayerListItem component test suite', (): void => {
+  it('should receive correct props for each layer', (): void => {
+    const { layerControllerLayers } = testData
+    for (const { id, isActive, name } of layerControllerLayers) {
+      const wrapper = mount(LayerControllerLayerListItem, {
+        props: {
+          id,
+          isActive,
+          name
+        }
+      })
+      expect(wrapper.props().id).toBe(id)
+      expect(wrapper.props().isActive).toBe(isActive)
+      expect(wrapper.props().name).toBe(name)
+    }
+  })
+})
